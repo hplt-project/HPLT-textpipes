0459b299543ee5db22c21ed2470a028476f34097
Stephan Oepen <oe@ifi.uio.no>
2025-11-12 00:04:07 +0100

    emerging glotlid support

--- src/hplt_textpipes/stage3/stage3download.sh
+++ src/hplt_textpipes/stage3/stage3download.sh
@@ -2,3 +2,4 @@ DDIR=~/.cache/hplt
 mkdir -p $DDIR
 
 wget -P $DDIR https://zenodo.org/records/15056559/files/openlid_v2_180325.bin
+curl --location -o ${DDIR}/glotlid-v3.bin https://huggingface.co/cis-lmu/glotlid/resolve/main/model_v3.bin?download=true
