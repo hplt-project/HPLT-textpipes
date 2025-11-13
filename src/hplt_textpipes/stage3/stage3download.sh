75369a73513403f48b5d583ef517c7921c2b4f88
MariaFjodorowa <laks.salmon@gmail.com>
2025-11-13 19:32:06 +0100

    upd model name on zenodo

--- src/hplt_textpipes/stage3/stage3download.sh
+++ src/hplt_textpipes/stage3/stage3download.sh
@@ -1,5 +1,5 @@
 DDIR=~/.cache/hplt
 mkdir -p $DDIR
 
-curl -o ${DDIR}/openlid-v3.bin https://zenodo.org/records/17593102/files/openlid_v3_model.bin
+curl -o ${DDIR}/openlid-v3.bin https://zenodo.org/records/17601701/files/openlid-v3.bin
 curl --location -o ${DDIR}/glotlid-v3.bin https://huggingface.co/cis-lmu/glotlid/resolve/main/model_v3.bin?download=true
