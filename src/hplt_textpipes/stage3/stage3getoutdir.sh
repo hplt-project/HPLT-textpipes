05d1bd116841071c92d764bc97235aef03665cb9
nvanva <ezhick179@gmail.com>
2025-11-17 01:00:23 +0100

    minor fixes

--- src/hplt_textpipes/stage3/stage3getoutdir.sh
+++ src/hplt_textpipes/stage3/stage3getoutdir.sh
@@ -2,4 +2,4 @@
 
 FIN=$1
 OUTDIR=$2
-echo $FIN | sed -r "s@^.*([^/]+)/html/([0-9]+)/text.zst@${OUTDIR%/}/\1/pool/\2@"
+echo $FIN | sed -r "s@^.*[/:]([^/:]+)/html/([0-9]+)/text.zst@${OUTDIR%/}/\1/pool/\2@"
