48af89aa174af9154df1ef06ebf88204001db756
nvanva <nikolare@uio.no>
2025-11-20 20:10:22 +0200

    bugfixes in the pipe monitoring code

--- threeone/stage3run/monitor_load.sh
+++ threeone/stage3run/monitor_load.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ps -fa | grep arefev| awk '{sum+=$4; n+=1} END {print n" "sum"%"}'
-for x in glotlid openlid xml2md ^zstd muxdemux ^t2sz ^rclone ^perl; do
+for x in '^python.*glotlid' '^python.*openlid' '^python.*xml2md' ^zstd '^python.*muxdemux' ^t2sz ^rclone ^perl; do
     PIDS=$(pgrep -f $x)
     if [[ -z "$PIDS" ]]; then
         continue
     fi
-    printf "%10s" "$x "; ps -up $PIDS | grep -vE '^UID|perl' | awk '{cpu+=$3; mem+=$4; vsz+=$5; n+=1} END {print n"\t"cpu"%\t"mem"%\t"vsz/1024/1024}'
+    printf "%20s" "$x "; ps -up $PIDS | grep -vE 'PID' | awk '{cpu+=$3; mem+=$4; vsz+=$5; n+=1} END {print n"\t"cpu"%\t"mem"%\t"vsz/1024/1024}' | column -t -s' '
+
 done
